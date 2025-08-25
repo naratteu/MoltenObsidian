461dbe2d43aa2adf9ac4e86989a14081ed8b1bea
naratteu <skfhddlg@naver.com>
2025-08-25 19:52:37 +0900

    github wiki test

--- Samples/Nodsoft.MoltenObsidian.Samples.HttpWasm/Program.cs
+++ Samples/Nodsoft.MoltenObsidian.Samples.HttpWasm/Program.cs
@@ -9,7 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddMoltenObsidianHttpAsyncVault("https://naratteu.github.io/daangn-garden/");
+builder.Services.AddMoltenObsidianHttpAsyncVault("https://raw.githubusercontent.com/wiki/naratteu/jurassic/");
 builder.Services.AddMoltenObsidianBlazorIntegration();
 
 await builder.Build().RunAsync();
